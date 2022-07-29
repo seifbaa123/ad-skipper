4034056a2a020c2e94002b8e04b2745223e68b66
seif <baazouziseif9@gmail.com>
2022-07-29 11:11:56 +0100

    show black box when ytp-ad-preview-container is in the screen

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
+const blackBox = document.createElement("div")
+blackBox.style = "position: fixed; top: 0; left: 0; width: 100%; height: 100vh; background-color: #000; z-index: 1000"
+
+const randomID = `black-box-random-id-${Math.floor(Math.random() * 10000)}`
+blackBox.setAttribute("id", randomID)
+
 setInterval(() => {
   const adSkipButton = document.querySelector(".ytp-ad-skip-button-container")
   if(adSkipButton != null) {
     adSkipButton.click()
   }
-}, 500)
+
+  const adPreview = document.querySelector(".ytp-ad-preview-container")
+  if(adPreview != null) {
+    document.body.appendChild(blackBox)    
+  } else {
+    blackBox.remove()
+  }
+}, 1000)
