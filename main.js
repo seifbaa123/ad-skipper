b0f2ab2d3cfb3c4a4c31d35dcfe6c986a2167d53
seif <baazouziseif9@gmail.com>
2022-07-29 10:50:30 +0100

    skip ad

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-console.log("Hello From ad-skipper")
+setInterval(() => {
+  const adSkipButton = document.querySelector(".ytp-ad-skip-button-container")
+  if(adSkipButton != null) {
+    adSkipButton.click()
+  }
+}, 500)
